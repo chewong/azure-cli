c98e2bb449a6bf33efd1f95994cf2bdef978a0d9
Ernest Wong <t-chwong@microsoft.com>
2018-01-22 15:18:37 -0800

    Incorporate environment variables

--- src/azure-cli-core/azure/cli/core/alias.py
+++ src/azure-cli-core/azure/cli/core/alias.py
@@ -14,8 +14,10 @@ GLOBAL_CONFIG_DIR = get_config_dir()
 ALIAS_FILE_NAME = 'alias'
 GLOBAL_ALIAS_PATH = os.path.join(GLOBAL_CONFIG_DIR, ALIAS_FILE_NAME)
 PLACEHOLDER_REGEX = r'\s*{\d+}'
+ENV_VAR_REGEX = r'\$[a-zA-Z][a-zA-Z0-9]*'
 
 logger = get_logger(__name__)
+
 
 class AliasTransformer:
 
@@ -26,36 +28,37 @@ class AliasTransformer:
         self.alias_map.read(GLOBAL_ALIAS_PATH)
 
     def transform(self, args):
-        """ Transform aliases to their respective commands """
+        """ Transform any aliases in args to their respective commands """
         transformed = []
-        enum_iter = enumerate(args, 1)
+        args_iter = enumerate(args, 1)
 
-        for i, arg in enum_iter:
+        for i, arg in args_iter:
             num_positional_args = self.count_positional_args(arg)
 
             if num_positional_args == 0:
+                # Call split() because the command that the alias points to might contain spaces
                 transformed += self.alias_map[arg]['command'].split() if arg in self.alias_map else [arg]
             else:
                 command = self.alias_map[self.get_full_alias(arg)]['command']
-                for placeholder, replacement in self.build_pos_args_map(args[i:i+num_positional_args]):
+                for placeholder, replacement in self.build_pos_args_map(args[i: i + num_positional_args]):
                     command = command.replace(placeholder, replacement)
-                    # Skip the next iteration because it is consumed as a positional argument
-                    next(enum_iter)
+                    # Skip the next iteration because it is already consumed as a positional argument above
+                    next(args_iter)
 
                 transformed += command.split()
 
         logger.debug(
             'Alias Transfromer: Command Arguments Transformed From %s to %s', args, transformed)
 
+        transformed = self.inject_env_vars(transformed)
         return transformed
 
     def count_positional_args(self, alias):
-        """ Count how many positional arguments there are in an alias.
-        Can be replaced by defining a new field in ini file """
-        return len(re.findall(r'{\d+}', self.get_full_alias(alias)))
+        """ Count how many positional arguments there are in an alias. """
+        return len(re.findall(PLACEHOLDER_REGEX, self.get_full_alias(alias)))
 
     def get_full_alias(self, query):
-        """ Return the full alias (with placeholder, if any) given a query """
+        """ Return the full alias (with the placeholders, if any) given a query """
         if query in self.alias_map:
             return query
         for section in self.alias_map.sections():
@@ -63,9 +66,16 @@ class AliasTransformer:
                 return section
         return ''
 
-    def build_pos_args_map(self, args):
+    def build_pos_args_map(self, args):  # pylint: disable=no-self-use
         """
-        Build and return a dictionary where the key is the positional argument placeholder and the value is
-        the argument value
+        Build and return a tuple of tuples ([0], [1]) where the [0] is the positional argument
+        placeholder and [1] is the argument value. e.g. ('{0}', pos_arg_1), ('{1}', pos_arg_2) ... )
         """
-        return {'{{{}}}'.format(i) : arg for i, arg in enumerate(args)}.items()
+        return tuple(('{{{}}}'.format(i), arg) for i, arg in enumerate(args))
+
+    def inject_env_vars(self, args):  # pylint: disable=no-self-use
+        command = ' '.join(args)
+        env_vars = re.findall(ENV_VAR_REGEX, command)
+        for env_var in env_vars:
+            command = command.replace(env_var, os.path.expandvars(env_var))
+        return command.split()
