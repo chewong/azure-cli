674c2ea009b2705abcba2237787c15e37b5fedd6
Ernest Wong <t-chwong@microsoft.com>
2018-01-24 12:24:59 -0800

    Working on alias collision detection

--- src/azure-cli-core/azure/cli/core/alias.py
+++ src/azure-cli-core/azure/cli/core/alias.py
@@ -16,38 +16,66 @@ ALIAS_FILE_NAME = 'alias'
 GLOBAL_ALIAS_PATH = os.path.join(GLOBAL_CONFIG_DIR, ALIAS_FILE_NAME)
 
 PLACEHOLDER_REGEX = r'\s*{\d+}'
+PLACEHOLDER_SPLIT_REGEX = r'\s*{0\.split\(((\'.*\')|(".*"))\)\[\d+\]}'
 ENV_VAR_REGEX = r'\$[a-zA-Z][a-zA-Z0-9]*'
+COMMAND_REGEX = r'[a-z\-]+'
 
 logger = get_logger(__name__)
 
 
 class AliasTransformer:
 
-    def __init__(self):
+    def __init__(self, reserved_commands):
         from configparser import ConfigParser
 
         self.alias_map = ConfigParser()
         self.alias_map.read(GLOBAL_ALIAS_PATH)
+        self.reserved_commands = reserved_commands
+        self.collision_regex = '^'
+        self.collision_cache = set()
+
 
     def transform(self, args):
         """ Transform any aliases in args to their respective commands """
         transformed = []
         args_iter = enumerate(map(str.lower, args), 1)
 
-        for i, arg in args_iter:
-            num_positional_args = self.count_positional_args(arg)
+        def check_collision(arg):
+            if arg[0] == '-':
+                return True
 
-            if num_positional_args == 0:
-                # Call split() because the command that the alias points to might contain spaces
-                transformed += self.alias_map[arg]['command'].split() if arg in self.alias_map else [arg]
+            self.collision_regex += '{} '.format(arg)
+            collided = list(filter(re.compile(self.collision_regex).match, self.reserved_commands))
+
+            if collided:
+                transformed.append(arg)
+                self.collision_cache.add(arg)
+                return True
             else:
-                command = self.alias_map[self.get_full_alias(arg)]['command']
-                for placeholder, replacement in self.build_pos_args_map(args[i: i + num_positional_args]):
-                    command = command.replace(placeholder, replacement)
+                self.collision_regex = self.collision_regex.replace(arg, COMMAND_REGEX)
+            return False
+
+        for i, arg in args_iter:
+            num_pos_args = self.count_positional_args(arg)
+
+            if num_pos_args == 0:
+                if check_collision(arg):
+                    continue
+
+                # Call split() because the command that the alias points to might contain spaces
+                transformed += self.alias_map[arg].get('command', arg).split() if arg in self.alias_map else [arg]
+            else:
+                full_alias = self.get_full_alias(arg)
+                command = self.alias_map[full_alias].get('command', arg) if full_alias in self.alias_map else arg
+                # Take arguments indexed from i to i + num_pos_args and inject
+                # them as positional arguments into the command
+                for placeholder, pos_arg in self.build_pos_args_map(args[i: i + num_pos_args]):
+                    command = command.replace(placeholder, pos_arg)
                     # Skip the next iteration because it is already consumed as a positional argument above
                     next(args_iter)
 
                 transformed += command.split()
+                break
 
         transformed = self.inject_env_vars(transformed)
 
@@ -58,12 +86,8 @@ class AliasTransformer:
 
         return transformed
 
-    def count_positional_args(self, alias):
-        """ Count how many positional arguments there are in an alias. """
-        return len(re.findall(PLACEHOLDER_REGEX, self.get_full_alias(alias)))
-
     def get_full_alias(self, query):
-        """ Return the full alias (with the placeholders, if any) given a query """
+        """ Return the full alias (with the placeholders, if any) given a search query """
         if query in self.alias_map:
             return query
         for section in self.alias_map.sections():
@@ -71,10 +95,17 @@ class AliasTransformer:
                 return section
         return ''
 
+    def get_command_from_alias(self, alias):
+        pass
+
+    def count_positional_args(self, alias):
+        """ Count how many positional arguments there are in an alias. """
+        return len(re.findall(PLACEHOLDER_REGEX, self.get_full_alias(alias)))
+
     def build_pos_args_map(self, args):  # pylint: disable=no-self-use
         """
         Build and return a tuple of tuples ([0], [1]) where the [0] is the positional argument
-        placeholder and [1] is the argument value. e.g. ('{0}', pos_arg_1), ('{1}', pos_arg_2) ... )
+        placeholder and [1] is the argument value. e.g. (('{0}', pos_arg_1), ('{1}', pos_arg_2) ... )
         """
         return tuple(('{{{}}}'.format(i), arg) for i, arg in enumerate(args))
 
@@ -89,5 +120,5 @@ class AliasTransformer:
     def check_recursive_alias(self, commands):
         """ Check for any recursive alias """
         for subcommand in commands:
-            if self.get_full_alias(subcommand):
+            if subcommand not in self.collision_cache and self.get_full_alias(subcommand):
                 raise CLIError('Potentially recursive alias: \'{}\' is referred by another alias'.format(subcommand))
